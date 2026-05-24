888abded851ccdf70ce14188686ac7732fd73305
SeleiXi <seleixi@seleixi.com>
2026-05-24 16:52:02 +0800

    Initial import

---
+++ src/app/settings/defaultSettings.ts
@@ -0,0 +1,52 @@
+import { getDefaultHiddenProviderCommands } from '../../core/providers/commands/hiddenCommands';
+import { type ClaudianSettings } from '../../core/types/settings';
+import { getBuiltInProviderDefaultConfigs } from '../../providers/defaultProviderConfigs';
+
+export const DEFAULT_CLAUDIAN_SETTINGS: ClaudianSettings = {
+  userName: '',
+
+  permissionMode: 'yolo',
+
+  model: 'haiku',
+  thinkingBudget: 'off',
+  effortLevel: 'high',
+  serviceTier: 'default',
+  enableAutoTitleGeneration: true,
+  titleGenerationModel: '',
+
+  excludedTags: [],
+  mediaFolder: '',
+  systemPrompt: '',
+  persistentExternalContextPaths: [],
+
+  sharedEnvironmentVariables: '',
+  envSnippets: [],
+  customContextLimits: {},
+
+  keyboardNavigation: {
+    scrollUpKey: 'w',
+    scrollDownKey: 's',
+    focusInputKey: 'i',
+  },
+
+  locale: 'en',
+
+  providerConfigs: getBuiltInProviderDefaultConfigs(),
+
+  settingsProvider: 'claude',
+  savedProviderModel: {},
+  savedProviderEffort: {},
+  savedProviderServiceTier: {},
+  savedProviderThinkingBudget: {},
+  savedProviderPermissionMode: {},
+
+  lastCustomModel: '',
+
+  maxTabs: 3,
+  tabBarPosition: 'input',
+  enableAutoScroll: true,
+  deferMathRenderingDuringStreaming: true,
+  chatViewPlacement: 'right-sidebar',
+
+  hiddenProviderCommands: getDefaultHiddenProviderCommands(),
+};
